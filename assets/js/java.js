9be4f1bf06d4697adb6949f1741ea9eff557a0d3
Amavi-rat <amavirathnayake@gmail.com>
2025-11-20 23:16:57 +0530

    Final Weather Application

--- assets/js/java.js
+++ assets/js/java.js
@@ -4,9 +4,11 @@ function searchWeather(){
 
     if(location===""){
         alert("Please enter a location");
+        return;
     }
 
-    let api_url='http://api.weatherapi.com/v1/current.json?key=...&q=${location}&&aqi=no';
+    let api_url = `http://api.weatherapi.com/v1/current.json?key=8ba518ce004e463898d173240252011&q=${location}&aqi=no`;
+
 
     fetch(api_url)
     .then(res => res.json())
@@ -19,17 +21,18 @@ function searchWeather(){
     document.getElementById("temperature").textContent= data.current.temp_c + " °C";
     document.getElementById("weather_state").textContent= data.current.condition.text;
 
-    document.getElementById("sunstate2").textContent= data.location.name;
-    document.getElementById("sunstate1").textContent= data.location.name;
+   
 
     document.getElementById("txthumidity").textContent= data.current.humidity + " %";
     document.getElementById("txtpressure").textContent= data.current.pressure_mb + " mb";
     document.getElementById("txtuv").textContent= data.current.uv;
     document.getElementById("txttemp").textContent= data.current.feelslike_c + " °C";
-    document.getElementById("txtwind").textContent= data.current.feelslike_c +"km/h";
+    document.getElementById("txtwind").textContent= data.current.wind_kph +"km/h";
 
     document.getElementById("state").src= data.current.condition.icon; 
+    document.getElementById("current_temp").textContent= data.current.temp_c + " °C"; 
 
-      })
+
+      });
     }
 
